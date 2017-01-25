30663374279411a3081e365dfe66d8d3e1b35e9c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-25 12:54:00 -0800

    Added student project details to student info page. Code complete.

--- hackbright-web.py
+++ hackbright-web.py
@@ -27,10 +27,13 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
+
+    rows = hackbright.get_grades_by_github(github)
     html = render_template("student_info.html",
                            first=first,
                            last=last,
-                           github=github)
+                           github=github,
+                           rows=rows)
     return html
 
 
